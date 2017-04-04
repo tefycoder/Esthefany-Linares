eb68c67f2fe0ce103c28b6e54d99cfc037e37bbb
tefy <tefysud21@gmail.com>
2017-04-04 00:52:00 -0500

    modificacion habilidades

--- js/main.js
+++ js/main.js
@@ -58,3 +58,23 @@ var menuHidden = function(){
 }
 
 $(document).ready(cargarPagina);
+
+//habilidades 
+jQuery(document).ready(function(){
+  
+  jQuery('.progress-bar').each(function() {
+    jQuery(this).find('.progress-content').animate({
+      width:jQuery(this).attr('data-percentage')
+    },2000);
+    
+    jQuery(this).find('.progress-number-mark').animate(
+      {left:jQuery(this).attr('data-percentage')},
+      {
+       duration: 2000,
+       step: function(now, fx) {
+         var data = Math.round(now);
+         jQuery(this).find('.percent').html(data + '%');
+       }
+    });  
+  });
+});
